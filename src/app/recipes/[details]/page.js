e1d69f668999f8c7dd79efacdee491a84d27cf2f
GIRI GIRIDHAR <girigiridhar0603@gmail.com>
2024-08-14 16:26:04 +0530

    first-commit

---
+++ src/app/recipes/[details]/page.js
@@ -0,0 +1,72 @@
+import Link from "next/link";
+
+const singleRecipe = async(currentId) =>{
+     try {
+        const response = await fetch(`https://dummyjson.com/recipes/${currentId}`);
+        const data = await response.json();
+        return data
+     } catch (error) {
+        throw new Error(error)
+     }
+}
+
+
+const recipeDetails = async ({params}) => {
+    const id = params.details;
+    const singleRecipeDetails = await singleRecipe(id);
+  return (
+    <div className="w-[95%] md:w-[85%] lg:w-[95%]  max-w-[1500px] mx-auto mt-10">
+       {
+        <div className="flex gap-8 lg:gap-5 xl:gap-10 flex-col lg:flex-row">
+            <div className="w-[100%] lg:w-[500px] h-[300px] md:h-[500px] overflow-hidden rounded-lg">
+                <img className="w-[100%] h-[100%] object-cover" src={singleRecipeDetails?.image} alt={singleRecipeDetails?.name} />
+            </div>
+            <div className=" flex flex-col gap-4">
+                 <div className=" text-2xl md:text-4xl font-bold">
+                     {singleRecipeDetails?.name}
+                 </div>
+                 <div>
+                     <span className="font-bold text-lg">Ingredients :</span>
+                     {singleRecipeDetails?.ingredients.map((item,index)=>(
+                        <ul key={index} className="mt-1 text-gray-500 font-bold">
+                            <li>{item}</li>
+                        </ul>
+                     ))}
+                 </div>
+
+                 <div>
+                 <span className="font-bold text-lg">Instructions :</span>
+                     {singleRecipeDetails?.instructions.map((item,index)=>(
+                         <ul key={index} className="mt-1 text-gray-500 font-bold">
+                             <li>"{item}"</li>
+                         </ul>
+                     ))}
+                 </div>
+
+                 <div>
+                 <span className="font-bold text-lg">Cooking Time :</span> <span className="text-gray-500 font-bold">{singleRecipeDetails?.cookTimeMinutes}mins</span>
+                 </div>
+
+                 <div>
+                 <span className="font-bold text-lg">Calories Per Serving :</span> <span className="text-gray-500 font-bold">{singleRecipeDetails?.caloriesPerServing}gm</span>
+                 </div>
+
+                 <div>
+                   <span className="font-bold text-lg">Meal Type :</span> {singleRecipeDetails?.mealType.map((item,index)=>(<span className="text-gray-500 font-bold px-1" key={index}>{item}</span>))}
+                 </div>
+
+                 <div>
+                    <span className="font-bold text-lg">Rating : </span> <span className="text-gray-500 font-bold">{singleRecipeDetails?.rating}/5</span>
+                 </div>
+                 
+                 <Link href={'/recipes'}>
+                 <button className="bg-blue-400 px-5 py-3 text-white tracking-widest uppercase font-bold rounded-lg">Go Back</button>
+                 </Link>
+            </div>
+        </div>
+       }
+    </div>
+  )
+}
+
+export default recipeDetails
---
+++ src/app/recipes/page.js
@@ -0,0 +1,59 @@
+import Link from "next/link";
+
+
+
+
+async function fetchRecipesData(){
+     try {
+        const response = await fetch(`https://dummyjson.com/recipes`);
+        const data = await response.json();
+        return data.recipes
+     } catch (error) {
+        throw new Error(error)
+     }
+}
+
+
+
+const recipes = async() => {
+  
+   const recipes = await fetchRecipesData();
+   console.log(recipes)
+
+  return (
+    <div className="w-[90%]  max-w-[1500px]  mx-auto">
+        <h1 className="text-center text-5xl font-bold mt-10">Food Recipes</h1>
+       
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 sm:gap-8 md:gap-3 lg:gap-5 xl:gap-8 my-5 space-y-5 md:space-y-0">
+             {recipes && recipes.length > 0 ? 
+                  recipes?.map(recipe=>(
+                    <Link href={`/recipes/${recipe.id}`}>
+                          <div key={recipe.id} className="bg-[rgb(245,246,247)]  flex flex-col  gap-4 p-5 rounded-lg overflow-hidden shadow-md">
+                          <div className="w-[100%] h-[350px] mx-auto overflow-hidden round">
+                            <img className="w-[100%] h-[100%] object-cover" src={recipe.image} alt={recipe.name} />
+                          </div>
+                          <div className="font-bold text-2xl text-slate-500">
+                             {recipe.name}
+                          </div>
+                           <div>
+                           <span className="font-bold"> Calories Per Serving :</span> <span className="text-green-500 font-bold">{recipe.caloriesPerServing}gm</span>
+                           </div>
+
+                           <div className="flex justify-between">
+                            <div>
+                           <span className="font-bold"> Rating : </span> <span className="text-yellow-500 font-bold">{recipe.rating}</span>
+                            </div>
+                             <div className="font-bold text-slate-600">
+                                {recipe.cuisine}
+                             </div>
+                           </div>
+                      </div>
+                    </Link>
+                  ))
+              : null}
+        </div>
+    </div>
+  )
+}
+
+export default recipes
